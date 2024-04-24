cc95938cf224e9008010e5f97cc95fa4420fa5ef
Adrian Brown <153460878+adrianbrown732@users.noreply.github.com>
2024-04-24 18:37:02 -0400

    Animate mobile overlay

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const hamMenuButton = document.querySelector(".ham-menu");
 const hamMenuPopout = document.querySelector(".ham-menu-popout");
-const bgOverlay = document.querySelector(".bg-overlay");
+const mblOverlay = document.querySelector(".mbl-overlay");
 
 hamMenuButton.addEventListener("click", () => {
-  hamMenuPopout.classList.toggle("hide");
-  //   bgOverlay.classList.toggle("mbl-overlay");
+  hamMenuPopout.classList.toggle("show-menu");
+  mblOverlay.classList.toggle("show-overlay");
 });
