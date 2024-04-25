229930deb277f5e87a30927af64864db0d805608
Adrian Brown <153460878+adrianbrown732@users.noreply.github.com>
2024-04-25 14:12:40 -0400

    Animate hamburger button

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 const hamMenuButton = document.querySelector(".ham-menu");
+const hamMenu = document.querySelector(".ham-menu");
 const hamMenuPopout = document.querySelector(".ham-menu-popout");
 const mblOverlay = document.querySelector(".mbl-overlay");
 
 hamMenuButton.addEventListener("click", () => {
   hamMenuPopout.classList.toggle("show-menu");
+  hamMenu.classList.toggle("move-lines");
   mblOverlay.classList.toggle("show-overlay");
 });
